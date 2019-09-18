ae1910fcc53d4d4c6bbe64432c8d57efd6618a04
JayAwesome <jayawesomest@gmail.com>
2019-09-18 20:56:53 +0100

    Initial commit

---
+++ app/src/main/java/com/pluralsight/loginandregistration/MainActivity.java
@@ -0,0 +1,23 @@
+package com.pluralsight.loginandregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final EditText etName = (EditText) findViewById(R.id.etName);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+
+    }
+}
